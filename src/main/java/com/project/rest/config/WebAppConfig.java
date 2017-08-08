2b8b384fe34558a95fbfc19deffab6f400871fc8
Denis <nalivko.den1726@mail.ru>
2017-08-08 23:55:53 +0300

    Was added: com.project.rest.config.Initializer.java and com.project.rest.config.WebAppConfig.java to initialize Spring RestFull Server
    com.project.rest.controller.MainController.java to make mapping for url
    
    com.project.Constant.java to store the constant
    
    and pom.xml was updated

---
+++ src/main/java/com/project/rest/config/WebAppConfig.java
@@ -0,0 +1,32 @@
+package com.project.rest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import com.project.Constant;
+
+// Говорим, что это конфигурация
+@Configuration
+// Включаем MVC
+@EnableWebMvc
+// Указываем где искать контроллеры и остальные компоненты
+@ComponentScan(Constant.COMPONENT_PACKAGE)
+public class WebAppConfig {
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        // указываем где будут лежать наши веб-страницы
+        resolver.setPrefix(Constant.VIEW_FOLDER);
+        // формат View который мы будем использовать
+        resolver.setSuffix(Constant.VIEW_FORMAT);
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
+    }
+
+}
