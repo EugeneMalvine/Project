79973b736e7922a24d217bde9bbeaaaba3c247cf
Denis <nalivko.den1726@mail.ru>
2017-08-10 20:22:29 +0300

    getall, delete, insert functional to controller

---
+++ src/main/java/com/project/rest/controller/WorkController.java
@@ -0,0 +1,77 @@
+package com.project.rest.controller;
+
+import com.project.service.PersonService;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import com.project.domain.Person;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/work")
+public class WorkController {
+
+    PersonService personMapper;
+    {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:ApplicationContext.xml");
+        ctx.refresh();
+
+        personMapper = ctx.getBean("personService", PersonService.class);
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<Person> getUsers() {
+        return personMapper.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public void addContact(@RequestBody Person contact) {
+        personMapper.save(contact);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    @ResponseBody
+    public void deleteContact(@RequestBody String id) {
+
+        try {
+            Long n_id = Long.parseLong(id ,10);
+            Person toFind = personMapper.findById(n_id);
+            if( toFind != null)
+                personMapper.delete(toFind);
+        }
+        catch(Exception e){
+
+        }
+    }
+
+
+
+
+    /*@RequestMapping(method = RequestMethod.PUT)
+    @ResponseBody
+    public MyDataObject putMyData(@RequestBody MyDataObject md) {
+        return md;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public MyDataObject postMyData() {
+        return new MyDataObject(Calendar.getInstance(), "это ответ метода POST!");
+    }
+
+    @RequestMapping(value= "/{time}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public MyDataObject deleteMyData(@PathVariable long time) {
+        return new MyDataObject(Calendar.getInstance(), "Это ответ метода DELETE!");
+    }*/
+}
